a5c19c4013550cd0748020fbe91134cff823378c
SodiumCXI <138525824+SodiumCXI@users.noreply.github.com>
2026-03-11 23:12:47 +0300

    chore: implement pinSHA256 in h2

--- install.sh
+++ install.sh
@@ -142,7 +142,12 @@ echo "Done. h2 installed to $MANAGER_PATH"
 
 SNI="${MASQ_URL#https://}"; SNI="${SNI%/}"
 URI="hysteria2://${AUTH_PASS}@${SERVER_IP}:${PORT}?sni=${SNI}&obfs=salamander&obfs-password=${OBFS_PASS}"
-[ "$CERT_MODE" != "2" ] && URI="${URI}&insecure=1"
+if [ "$CERT_MODE" = "2" ]; then
+  PIN=$(openssl x509 -in /etc/hysteria/server.crt -outform DER | openssl dgst -sha256 -binary | openssl base64)
+  URI="${URI}&pinSHA256=${PIN}"
+else
+  URI="${URI}&insecure=1"
+fi
 URI="${URI}#${KEY_NAME}"
 
 echo ""
